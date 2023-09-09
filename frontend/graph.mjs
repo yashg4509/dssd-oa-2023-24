c4c1f047e363d3931fec6935617f0c4b98a64ff2
Jack Blake <jhblake2@wisc.edu>
2023-09-09 18:24:18 -0500

    update fetch URL

--- frontend/graph.mjs
+++ frontend/graph.mjs
@@ -1,7 +1,8 @@
 const graphDiv = document.getElementById("graph");
 
-fetch("http://localhost:3000")
-    .then(async res => {
-        Plotly.newPlot( graphDiv, [ await res.json() ]); 
-    })
+fetch(
+    "https://oa-2023-24-backend.onrender.com" //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
+).then(async res => {
+    Plotly.newPlot( graphDiv, [ await res.json() ]); 
+})
 
