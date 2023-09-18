e4ce0fd53495966c0b4c5f18ee264ad6e6a20b19
yashg4509 <bludgermoonstone66@gmail.com>
2023-09-17 23:41:33 -0500

    new image

--- frontend/graph.mjs
+++ frontend/graph.mjs
@@ -1,7 +1,7 @@
 const graphDiv = document.getElementById("graph");
 
-fetch("http://localhost:3000")
-    .then(async res => {
-        const data = await res.json();
-        Plotly.newPlot(graphDiv, [data]);
-    });
+fetch(
+    "https://dssd-oa-2023-2024-gupta.onrender.com/" //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
+).then(async res => {
+    Plotly.newPlot( graphDiv, [ await res.json() ]); 
+})
